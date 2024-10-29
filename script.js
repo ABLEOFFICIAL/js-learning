9bdd509e0db70767ac75c66fcfa7058c4bda54c9
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-10-29 13:14:33 -0700

    learning

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let gender= 'female';
-let tribe= 'USA'
+// if else statement
 
-if (gender === 'male'  && tribe === 'Africa'){
-    console.log('100% eligibility');
-}else if(gender === 'male' || tribe === 'Africa'){
-    console.log('80% eligibility');
-}else{
-    console.log('50% eligibility');
-}
+// let gender= 'female';
+// let tribe= 'USA'
+
+// if (gender === 'male'  && tribe === 'Africa'){
+//     console.log('100% eligibility');
+// }else if(gender === 'male' || tribe === 'Africa'){
+//     console.log('80% eligibility');
+// }else{
+//     console.log('50% eligibility');
+// }
+
+
+// switch statements
+// let access= 3;
+
+// switch(access){
+//     case 1:
+//         console.log('you are qualified')
+//         break;
+//     case 3:
+//         console.log('you are not qualified')
+//         break;
+//     default:
+//         console.log('message the admin')
+// }
+
+
+// tenary operators
+let age = 12;
+
+(age >= 18)?  console.log('you are eligible') : console.log('you are not eligible')
+
+age += 12;
+
+(age >= 18)?  console.log('you are eligible') : console.log('you are not eligible')
