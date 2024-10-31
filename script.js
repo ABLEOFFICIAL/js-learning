9d4dd6363d5d979aeb494ff084d4333176b46005
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-10-31 11:18:25 -0700

    learning

--- script.js
+++ script.js
@@ -66,6 +66,102 @@
 
 // for loop
 // (initializatin; condition; increment;)
-for(i = 2; i < 12; i= i + 2){
+/*for(i = 2; i < 12; i= i + 2){
     console.log('hello');
+}*/
+
+// for loop ('IF' in action)
+
+// for (i = 1; i < 11; i++){
+//     if(i === 4){
+//         console.log('this is the fourth number');
+//     }
+//     console.log('hello ' + i)
+// }
+
+// break statement
+// for (i = 1; i < 11; i++){
+//     if(i === 4){
+//         console.log('this is the fourth number');
+//         break;
+//     }
+//     console.log('hello ' + i)
+// }
+
+// continue statement
+// for (i = 1; i < 11; i++){
+//     if(i === 4){
+//         console.log('this is the fourth number');
+//         continue;
+//     }
+//     console.log('hello ' + i)
+// }
+
+// looping through an array
+// let myArray = ['orange', 'grape', 'mangoes', 'banana']
+
+// myArray.push('carrot');
+// myArray.pop();
+// myArray.shift();
+// myArray.unshift('pepper');
+
+/*for(let i = 0; i < myArray.length; i++){
+    // var currentvalue = myArray[i];
+    console.log(myArray[i]);
+}*/
+
+
+// looping through multidimensional arrays
+
+/*var multArr = [
+    [1,2,3,4],
+    ['a','b','c','d','e']
+]
+
+for(let i=0; i<multArr.length; i++){
+    var child = multArr[i];
+    for(let j=0; j<multArr[i].length; j++)
+        console.log(multArr[i][j]);
+    // console.log(multArr[i].length);
+}*/
+
+// array examples
+
+// finding the highest score
+/*let scores = [30,25,95,70,90,49,66];
+var maxValue = -Number.MAX_VALUE;
+
+
+for(let i = 0; i < scores.length; i++){
+        if(scores[i] > maxValue){
+        maxValue = scores[i];
+    }
 }
+console.log(maxValue);*/
+
+// finding the lowest score
+let scores = [30,25,95,70,90,49,66];
+/*var minValue = Number.MAX_VALUE;
+
+
+for(let i = 0; i < scores.length; i++){
+        if(scores[i] < minValue){
+        minValue = scores[i];
+    }
+}
+console.log(minValue);*/
+
+// my intellect
+// checking if a particular score is in the array
+// let findValue = 50;
+// let valPresent = false;
+// for(let i = 0; i < scores.length; i++){
+//     if(findValue == scores[i]){
+//         valPresent = true;
+//     }
+// };
+// console.log(valPresent);
+
+scores.forEach(function(score){
+    console.log(score)
+})
