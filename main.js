23f3e7e91592e229828460ef9aea3eb8bc6b6049
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-05 02:19:19 -0800

    js lessons

---
+++ main.js
@@ -0,0 +1,36 @@
+// // function sumfunc(a=7,b=9){
+// //     let result = a + b;
+// //     console.log(result);
+// // }
+
+// // sumfunc();
+
+// function addNum(a,b){
+//     return a + b;
+// }
+
+// console.log(addNum(1,7));
+
+/*function person(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.fullName = function (){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+const person1 = new person('Josh', 'Daves', '29-09-2004');
+
+console.log(person1);
+// console.log(person1.fullName);
+console.log(`${this.firstName}`);*/
+
+// class person {
+//     constructor (firstName, lastName, dob) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+// }
+// const person1 = new person('Josh', 'Daves', '29-09-2004');
+// console.log(person1.fullName);
--- script.js
+++ script.js
@@ -1,167 +1,169 @@
-// if else statement
+// // if else statement
 
-// let gender= 'female';
-// let tribe= 'USA'
+// // let gender= 'female';
+// // let tribe= 'USA'
 
-// if (gender === 'male'  && tribe === 'Africa'){
-//     console.log('100% eligibility');
-// }else if(gender === 'male' || tribe === 'Africa'){
-//     console.log('80% eligibility');
-// }else{
-//     console.log('50% eligibility');
-// }
-
-
-// switch statements
-// let access= 3;
-
-// switch(access){
-//     case 1:
-//         console.log('you are qualified')
-//         break;
-//     case 3:
-//         console.log('you are not qualified')
-//         break;
-//     default:
-//         console.log('message the admin')
-// }
+// // if (gender === 'male'  && tribe === 'Africa'){
+// //     console.log('100% eligibility');
+// // }else if(gender === 'male' || tribe === 'Africa'){
+// //     console.log('80% eligibility');
+// // }else{
+// //     console.log('50% eligibility');
+// // }
 
 
-// tenary operators
-// let age = 12;
+// // switch statements
+// // let access= 3;
 
-// (age >= 18)?  console.log('you are eligible') : console.log('you are not eligible');
-
-// age += 12;
-
-// (age >= 18)?  console.log('you are eligible') : console.log('you are not eligible');
-
-// while loop
-// let num = 5;
-
-// while(num > 0){
-//     console.log("The number "+ num +" is greater than 0");
-//     num--;
-// }
-
-// function for guessing a number = Math.floor(Math.random() * 10) + 1;
-
-// let guessedNumber = Math.floor(Math.random() * 10) + 1;
-// let userInput = 5;
-
-// console.log(guessedNumber);
-// while(guessedNumber != userInput){
-//     console.log('unable to guess the number');
-//     guessedNumber = Math.floor(Math.random() * 10) + 1;
-//     console.log(guessedNumber);
-// }
-
-// do...while loop
-// let num = 4;
-
-// do{
-//     console.log('Ekwesi Joshua Chimeremeze')
-//     num--;
-// }while(num>5);
-
-// for loop
-// (initializatin; condition; increment;)
-/*for(i = 2; i < 12; i= i + 2){
-    console.log('hello');
-}*/
-
-// for loop ('IF' in action)
-
-// for (i = 1; i < 11; i++){
-//     if(i === 4){
-//         console.log('this is the fourth number');
-//     }
-//     console.log('hello ' + i)
-// }
-
-// break statement
-// for (i = 1; i < 11; i++){
-//     if(i === 4){
-//         console.log('this is the fourth number');
-//         break;
-//     }
-//     console.log('hello ' + i)
-// }
-
-// continue statement
-// for (i = 1; i < 11; i++){
-//     if(i === 4){
-//         console.log('this is the fourth number');
-//         continue;
-//     }
-//     console.log('hello ' + i)
-// }
-
-// looping through an array
-// let myArray = ['orange', 'grape', 'mangoes', 'banana']
-
-// myArray.push('carrot');
-// myArray.pop();
-// myArray.shift();
-// myArray.unshift('pepper');
-
-/*for(let i = 0; i < myArray.length; i++){
-    // var currentvalue = myArray[i];
-    console.log(myArray[i]);
-}*/
+// // switch(access){
+// //     case 1:
+// //         console.log('you are qualified')
+// //         break;
+// //     case 3:
+// //         console.log('you are not qualified')
+// //         break;
+// //     default:
+// //         console.log('message the admin')
+// // }
 
 
-// looping through multidimensional arrays
+// // tenary operators
+// // let age = 12;
 
-/*var multArr = [
-    [1,2,3,4],
-    ['a','b','c','d','e']
-]
+// // (age >= 18)?  console.log('you are eligible') : console.log('you are not eligible');
 
-for(let i=0; i<multArr.length; i++){
-    var child = multArr[i];
-    for(let j=0; j<multArr[i].length; j++)
-        console.log(multArr[i][j]);
-    // console.log(multArr[i].length);
-}*/
+// // age += 12;
 
-// array examples
+// // (age >= 18)?  console.log('you are eligible') : console.log('you are not eligible');
 
-// finding the highest score
-/*let scores = [30,25,95,70,90,49,66];
-var maxValue = -Number.MAX_VALUE;
+// // while loop
+// // let num = 5;
+
+// // while(num > 0){
+// //     console.log("The number "+ num +" is greater than 0");
+// //     num--;
+// // }
+
+// // function for guessing a number = Math.floor(Math.random() * 10) + 1;
+
+// // let guessedNumber = Math.floor(Math.random() * 10) + 1;
+// // let userInput = 5;
+
+// // console.log(guessedNumber);
+// // while(guessedNumber != userInput){
+// //     console.log('unable to guess the number');
+// //     guessedNumber = Math.floor(Math.random() * 10) + 1;
+// //     console.log(guessedNumber);
+// // }
+
+// // do...while loop
+// // let num = 4;
+
+// // do{
+// //     console.log('Ekwesi Joshua Chimeremeze')
+// //     num--;
+// // }while(num>5);
+
+// // for loop
+// // (initializatin; condition; increment;)
+// /*for(i = 2; i < 12; i= i + 2){
+//     console.log('hello');
+// }*/
+
+// // for loop ('IF' in action)
+
+// // for (i = 1; i < 11; i++){
+// //     if(i === 4){
+// //         console.log('this is the fourth number');
+// //     }
+// //     console.log('hello ' + i)
+// // }
+
+// // break statement
+// // for (i = 1; i < 11; i++){
+// //     if(i === 4){
+// //         console.log('this is the fourth number');
+// //         break;
+// //     }
+// //     console.log('hello ' + i)
+// // }
+
+// // continue statement
+// // for (i = 1; i < 11; i++){
+// //     if(i === 4){
+// //         console.log('this is the fourth number');
+// //         continue;
+// //     }
+// //     console.log('hello ' + i)
+// // }
+
+// // looping through an array
+// // let myArray = ['orange', 'grape', 'mangoes', 'banana']
+
+// // myArray.push('carrot');
+// // myArray.pop();
+// // myArray.shift();
+// // myArray.unshift('pepper');
+
+// /*for(let i = 0; i < myArray.length; i++){
+//     // var currentvalue = myArray[i];
+//     console.log(myArray[i]);
+// }*/
 
 
-for(let i = 0; i < scores.length; i++){
-        if(scores[i] > maxValue){
-        maxValue = scores[i];
-    }
-}
-console.log(maxValue);*/
+// // looping through multidimensional arrays
 
-// finding the lowest score
-let scores = [30,25,95,70,90,49,66];
-/*var minValue = Number.MAX_VALUE;
+// /*var multArr = [
+//     [1,2,3,4],
+//     ['a','b','c','d','e']
+// ]
+
+// for(let i=0; i<multArr.length; i++){
+//     var child = multArr[i];
+//     for(let j=0; j<multArr[i].length; j++)
+//         console.log(multArr[i][j]);
+//     // console.log(multArr[i].length);
+// }*/
+
+// // array examples
+
+// // finding the highest score
+// /*let scores = [30,25,95,70,90,49,66];
+// var maxValue = -Number.MAX_VALUE;
 
 
-for(let i = 0; i < scores.length; i++){
-        if(scores[i] < minValue){
-        minValue = scores[i];
-    }
-}
-console.log(minValue);*/
-
-// my intellect
-// checking if a particular score is in the array
-// let findValue = 50;
-// let valPresent = false;
 // for(let i = 0; i < scores.length; i++){
-//     if(findValue == scores[i]){
-//         valPresent = true;
+//         if(scores[i] > maxValue){
+//         maxValue = scores[i];
 //     }
-// };
-// console.log(valPresent);
+// }
+// console.log(maxValue);*/
 
-scores.forEach(function(score){
-    console.log(score)
-})
+// // finding the lowest score
+// let scores = [30,25,95,70,90,49,66];
+// /*var minValue = Number.MAX_VALUE;
+
+
+// for(let i = 0; i < scores.length; i++){
+//         if(scores[i] < minValue){
+//         minValue = scores[i];
+//     }
+// }
+// console.log(minValue);*/
+
+// // my intellect
+// // checking if a particular score is in the array
+// // let findValue = 50;
+// // let valPresent = false;
+// // for(let i = 0; i < scores.length; i++){
+// //     if(findValue == scores[i]){
+// //         valPresent = true;
+// //     }
+// // };
+// // console.log(valPresent);
+
+// scores.forEach(function(score){
+//     console.log(score)
+// })
+
+
