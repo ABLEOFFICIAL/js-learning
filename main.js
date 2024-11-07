969b0429346f31712491c891df7e4330deb4644a
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-07 10:48:19 -0800

    js learning

---
+++ main.js
@@ -0,0 +1,7 @@
+var header = document.querySelector('.logo');
+header.textContent = 'hello';
+var nameInput = document.querySelector('#text');
+nameInput.value = 'josh';
+var list = document.querySelector('.items');
+list.style.backgroundColor = 'yellow';
+// console.log(header);
