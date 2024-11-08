85767cb5d3a85bd7585ceae760241d913746040e
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-08 05:50:51 -0800

    learning

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-var newDiv = document.createElement('div');
-newDiv.className = 'itemListing';
 
-var divText = document.createTextNode('hello worlld')
-newDiv.appendChild(divText);
-var container = document.querySelector('.container');
-var add = document.querySelector('.add')
-
-
-console.log(newDiv);
