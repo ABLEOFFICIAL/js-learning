7c89e311d88bd38cb57c8f0baf9e432eda4224e3
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-09 00:16:22 -0800

    create an element

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+var form = document.getElementById('fornControl');
+var listItems = document.getElementById('listItems');
 
+
+form.addEventListener('submit', addListItem);
+
+function addListItem(e) {
+    e.preventDefault();
+
+
+    var inputValue = document.getElementById('text').value;
+    var newItem = document.createElement('li');
+    var newDiv = document.createElement('div');
+
+    newDiv.className = 'card';
+
+    // add class
+    newItem.className = 'items';
+
+    // append child
+    newDiv.appendChild(newItem);
+    newItem.appendChild(document.createTextNode(inputValue));
+
+    listItems.appendChild(newDiv);
+
+    var delbtn = document.createElement('button');
+    delbtn.className = 'btn';
+
+    var delvalue = document.createTextNode('x');
+    delbtn.appendChild(delvalue);
+
+    newDiv.appendChild(delbtn);
+
+
+    console.log(delbtn);
+
+    console.log(newItem);
+
+
+}
+// console.log(form);
